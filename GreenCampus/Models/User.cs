a7a74188612c52f004b3e3a6e1079d7c46b5f7e8
Jan Bernardić <47927018+jbernardic@users.noreply.github.com>
2025-12-11 09:09:38 +0100

    update user model

--- GreenCampus/Models/User.cs
+++ GreenCampus/Models/User.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace GreenCampus.Models
 {
     public partial class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
 
         public string FirstName { get; set; } = null!;
