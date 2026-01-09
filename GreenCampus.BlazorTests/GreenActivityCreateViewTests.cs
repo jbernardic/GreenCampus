2919b8bbe80aadf739dc01a2b9fc2a07cf9fc0a2
FlorijanSabljic <161614201+FlorijanSabljic@users.noreply.github.com>
2026-01-09 16:10:31 +0100

    tests

---
+++ GreenCampus.BlazorTests/GreenActivityCreateViewTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace GreenCampus.Tests
+{
+    [TestFixture]
+    public class GreenActivityCreateViewTests
+    {
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new WebApplicationFactory<Program>();
+            _client = _factory.CreateClient();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Test]
+        public async Task CreateActivityPage_ReturnsHtmlWithForm()
+        {
+            // Act
+            var response = await _client.GetAsync("/GreenActivities/Create");
+            var html = await response.Content.ReadAsStringAsync();
+
+            // Assert
+        }
+    }
+}
